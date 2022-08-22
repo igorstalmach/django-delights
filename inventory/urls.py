896e65d80734082be29e9c505f6d123026ec5df9
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2022-08-23 01:05:26 +0200

    Added new templates and fixed CSS
    Implemented Grid Layout

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-
+    path("", views.HomePageView.as_view(), name="home"),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Ingredient, Recipe, RecipeRequirement, Purchase
 
-# Create your views here.
+
+class HomePageView(LoginRequiredMixin, TemplateView):
+    template_name = "inventory/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ingredients'] = Ingredient.objects.all()
+        context['recipe'] = Recipe.objects.all()
+        context['purchases'] = Purchase.objects.all()
+        return context
