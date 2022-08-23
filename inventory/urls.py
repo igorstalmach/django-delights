379208e5f1309048b907d87beed9d77cc2b64bd8
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2022-08-23 19:37:43 +0200

    Added views and enhanced CSS

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", views.log_out, name="logout")
+    path("logout/", views.log_out, name="logout"),
+    path("ingredients/", views.ViewIngredients.as_view(), name="ingredients"),
+    path("ingredients/add_ingredient", views.AddIngredient.as_view(), name="add_ingredient"),
+    path("purchases/", views.ViewPurchase.as_view(), name="purchases"),
+    path("purchases/add_purchase/", views.AddPurchase.as_view(), name="add_purchase"),
+    path("recipes/", views.ViewRecipe.as_view(), name="recipes"),
+    path("recipes/add_recipe/", views.AddRecipe.as_view(), name="add_recipe"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import redirect
 from django.contrib.auth import logout
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
 from .models import Ingredient, Recipe, RecipeRequirement, Purchase
+from .forms import IngredientForm, RecipeForm, PurchaseForm
 
 
 def log_out(request):
@@ -18,4 +22,45 @@ class HomePageView(LoginRequiredMixin, TemplateView):
         context['ingredients'] = Ingredient.objects.all()
         context['recipes'] = Recipe.objects.all()
         context['purchases'] = Purchase.objects.all()
+
+        revenue = Purchase.objects.aggregate(revenue=Sum('item__price'))['revenue']
+        costs = Ingredient.objects.aggregate(costs=Sum('price_per_unit'))['costs']
+
+        context['revenue'] = revenue
+        context['costs'] = costs
+        context['profit'] = revenue - costs
+        
         return context
+
+
+class ViewIngredients(LoginRequiredMixin, ListView):
+    model = Ingredient
+    template_name = "inventory/list/list_ingredients.html"
+
+
+class AddIngredient(LoginRequiredMixin, CreateView):
+    model = Ingredient
+    template_name = "inventory/add/add_ingredient.html"
+    form_class = IngredientForm
+
+
+class ViewPurchase(LoginRequiredMixin, ListView):
+    model = Purchase
+    template_name = "inventory/list/list_purchases.html"
+
+
+class AddPurchase(LoginRequiredMixin, CreateView):
+    model = Purchase
+    template_name = "inventory/add/add_purchase.html"
+    form_class = PurchaseForm
+
+
+class ViewRecipe(LoginRequiredMixin, ListView):
+    model = Recipe
+    template_name = "inventory/list/list_recipes.html"
+
+
+class AddRecipe(LoginRequiredMixin, CreateView):
+    model = Recipe
+    template_name = "inventory/add/add_recipe.html"
+    form_class = RecipeForm
